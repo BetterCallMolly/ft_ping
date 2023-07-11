af34ec6bfbf2590029f2914c928a9acce3d714ef
BetterCallMolly <molly@mana.rip>
2023-07-11 22:12:49 +0200

    update: mimic inet's identifier

--- srcs/get_identifier.c
+++ srcs/get_identifier.c
@@ -5,5 +5,5 @@ uint16_t get_echo_identifier(void)
 {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (uint16_t)tv.tv_usec % 0xFFFF;
+    return (uint16_t)tv.tv_usec & 0x00FF; // I noticed that the identifier is only 1 byte in the original ping, so we'll do the same
 }
