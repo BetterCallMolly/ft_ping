6a85b44a2b296c95be27903ddb359a58a14247ee
BetterCallMolly <molly@mana.rip>
2023-07-11 22:39:28 +0200

    update: few hacks to not allocate memory

--- srcs/icmp.c
+++ srcs/icmp.c
@@ -3,9 +3,10 @@
 
 bool compute_icmp_checksum(t_icmp_packet *packet)
 {
-    int32_t sum = checksum((uint8_t *)(&packet->header), sizeof(packet->header));
-    packet->header.checksum = sum;
-    return true;
+    if (!packet)
+        return false;
+    packet->checksum = checksum(serialize_icmp_packet(packet), sizeof(t_icmp_packet));
+    return packet->checksum != 0;
 }
 
 /**
@@ -18,14 +19,14 @@ void disasm_icmp_packet(t_icmp_packet *packet, bool disasm_data)
 {
     if (!packet)
         return ;
-    if (packet->header.type != ECHO_REQUEST && packet->header.type != ECHO_REPLY)
+    if (packet->type != ECHO_REQUEST && packet->type != ECHO_REPLY)
         return ;
     printf("ICMP packet @ %p:\n", packet);
-    printf("    type: 0x%02x\n", packet->header.type);
-    printf("    code: 0x%02x\n", packet->header.code);
-    printf("    checksum: 0x%04x\n", packet->header.checksum);
-    printf("    identifier: 0x%02x 0x%02x\n", packet->header.rest[0], packet->header.rest[1]);
-    printf("    seq_number: 0x%02x 0x%02x\n", packet->header.rest[2], packet->header.rest[3]);
+    printf("    type: 0x%02x\n", packet->type);
+    printf("    code: 0x%02x\n", packet->code);
+    printf("    checksum: 0x%04x\n", packet->checksum);
+    printf("    identifier: 0x%02x 0x%02x\n", packet->rest[0], packet->rest[1]);
+    printf("    seq_number: 0x%02x 0x%02x\n", packet->rest[2], packet->rest[3]);
     printf("    timestamp: 0x%08x\n", packet->timestamp);
     if (!disasm_data)
         return ;
@@ -47,9 +48,5 @@ uint8_t *serialize_icmp_packet(t_icmp_packet *packet)
 {
     if (!packet)
         return NULL;
-    uint8_t *buffer = malloc(sizeof(t_icmp_header) + (sizeof(uint32_t) * 2) + packet->size);
-    if (!buffer)
-        return NULL;
-    memcpy(buffer, packet, sizeof(t_icmp_packet));
-    return buffer;
+    return (uint8_t *)&packet->checksum; // small hack to return the address of the first byte of the packet
 }
