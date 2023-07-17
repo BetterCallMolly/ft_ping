63b140ef12354d09882aa0e626ad2fefe7963320
BetterCallMolly <molly@mana.rip>
2023-07-17 15:08:35 +0200

    add: `s_summary` struct

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <netinet/ip.h>
+#include <signal.h>
 #include "icmp.h"
 
 #ifndef DEFAULT_DATA_SIZE
@@ -18,3 +19,24 @@
 #ifndef DEFAULT_MIN_DELAY
     #define DEFAULT_MIN_DELAY 1000000
 #endif
+
+#ifndef MAX_ARG_SIZE
+    #define MAX_ARG_SIZE 2000000 // 2MB
+#endif
+
+#ifndef MAX_SAMPLE_SIZE
+    #define MAX_SAMPLE_SIZE 1000 // over is just stupid
+#endif
+
+typedef struct s_summary {
+    char HOST_NAME[MAX_ARG_SIZE];
+    uint32_t sent;
+    uint32_t received;
+    uint32_t lost;
+    float min_delay;
+    float max_delay;
+    float delays[MAX_SAMPLE_SIZE];
+} t_summary;
+
+float avg(t_summary *summary);
+float stddev(t_summary *summary, float mean);
