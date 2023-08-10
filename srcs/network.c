ed4066c31451799acd294253ba700f25dd1355cc
BetterCallMolly <molly@mana.rip>
2023-08-10 13:27:28 +0200

    use: `recvmsg`

--- srcs/network.c
+++ srcs/network.c
@@ -1,12 +1,13 @@
 #include "ft_ping.h"
+#include <errno.h>
 
 /**
  * @brief Sends an ECHO_REQUEST packet to the specified destination
- * 
+ *
  * @param packet Packet to send
  * @param sockfd Socket file descriptor
  * @param dest Destination
- * @return ssize_t 
+ * @return ssize_t
  */
 ssize_t send_packet(t_icmp_packet *packet, int sockfd, struct sockaddr_in *dest)
 {
@@ -15,33 +16,45 @@ ssize_t send_packet(t_icmp_packet *packet, int sockfd, struct sockaddr_in *dest)
         serialize_icmp_packet(packet),
         EMPTY_PACKET_SIZE + packet->size,
         0,
-        (struct sockaddr *)dest, sizeof(*dest)
-    );
+        (struct sockaddr *)dest, sizeof(*dest));
 }
 
 /**
  * @brief Receives an ECHO_REPLY packet from the specified socket
- * 
+ *
  * @param sockfd Socket file descriptor
  * @param buffer Buffer to store the packet
  * @param size Size of the buffer
- * @return ssize_t 
+ * @return ssize_t
  */
 ssize_t receive_packet(int sockfd, uint8_t *buffer, size_t size)
 {
-    alarm(DEFAULT_TIMEOUT);
-    // TODO: Use recvmsg() instead of recvfrom()
-    ssize_t bytes_received = recvfrom(
-        sockfd,
-        buffer,
-        size,
-        0,
-        NULL, NULL
-    );
-    alarm(0);
-    if (bytes_received < 0) {
+    // alarm(DEFAULT_TIMEOUT);
+
+    struct iovec iov = {
+        .iov_base = buffer,
+        .iov_len = size
+    };
+
+    struct msghdr msg = {
+        .msg_name = NULL,
+        .msg_namelen = 0,
+        .msg_iov = &iov,
+        .msg_iovlen = 1,
+        .msg_control = NULL,
+        .msg_controllen = 0,
+        .msg_flags = 0
+    };
+
+    ssize_t bytes_received = recvmsg(sockfd, &msg, 0);
+
+    // alarm(0);
+
+    if (bytes_received < 0 && errno != EAGAIN)
+    {
         char *error = strerror(errno);
         fprintf(stderr, "ft_ping: recvmsg: %s\n", error);
     }
-    return bytes_received;
+
+    return errno == EAGAIN ? 0 : bytes_received;
 }
